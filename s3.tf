e8dce607440d3d5159d1d4e5ccb39b23c91ef5b6
SMIT KUDCHIKAR <smitkudchikar67@gmail.com>
2026-09-22 18:01:34 +0530

    Update S3 configuration

--- s3.tf
+++ s3.tf
@@ -9,13 +9,3 @@ resource "aws_s3_bucket" "cstm_bucket" {
 
 
 
-
-Terraform Init  => Intialise resources/ initialise modules
-
-Terraform fmt => Terraform Format
-
-Terrafrom Validate => Terraform check the syntax used is valid or not
-
-Terraform Plan => Blueprint of the terraform resources 
-
-Terraform apply => final command apply all the resources in the cloud
